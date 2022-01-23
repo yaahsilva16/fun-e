b07004485f07d17d77c89c5cc4dbb18e25674b55
Yasmin da Silva <95134741+yaahsilva16@users.noreply.github.com>
2022-01-22 22:20:55 -0300

    entregando meu exercicio funções

---
+++ script.js
@@ -0,0 +1,29 @@
+    function MostrarMensagem(mnsgm){
+    console.log(`olá!. tudo bem? como voce está?`)
+    }
+    
+    
+    function MostrarNome(nome){
+        console.log(`oi,${nome}. tudo bem?`)
+    }
+    MostrarNome(`Yasmin`)
+    
+    
+    function perfil(name,number,style){
+        console.log(`${name}, ${number}, ${style}`)
+    }
+    perfil(`Yasmin`, `16`, `indie`)
+    
+    
+    function perfil(film,music){
+        console.log(`oi, meu filme favorito é ${film}, e a minha musica favorita é ${music}`)
+    }
+    perfil(`Ela é demais`, `Vai passar`)
+    
+    
+    function double(num){
+        return num*8
+    }
+    double(8)
+    console.log(double(64))
+    
